ba90a07168c0dc760a71929f25662add0d23176b
Nahum Casco <nahumcasco.v@gmail.com>
2022-02-06 21:30:47 -0500

    correccion de slider

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect} from 'react';
+import { useState, useRef, useEffect } from 'react';
 import '../styles/slider.css';
 import avengers from '../assets/imagenes/avengers.jpg';
 import harry from '../assets/imagenes/harrypotter2.jpg';
@@ -7,42 +7,80 @@ import dune from '../assets/imagenes/dune.jpg';
 import SliderBanner from './SliderBanner';
 import { MdArrowForwardIos, MdOutlineArrowBackIosNew } from 'react-icons/md';
 
+const moviesMockup = [
+    {
+        id:'1',
+        title: 'Avengers EndGame',
+        banner: avengers
+    },
+    {
+        id:'2',
+        title: 'Spiderman No Way Home',
+        banner: spiderman
+    },
+    {
+        id:'3',
+        title: 'Harry Potter Reliquias del Muerte Parte 2',
+        banner: harry
+    },
+    {
+        id:'4',
+        title: 'Dune',
+        banner: dune
+    },
+]
+
 const Slider = () => {
     const slider = useRef(null);
     const [sliderLength, setSliderLength] = useState(0);
     const [currentSlider, setCurrentSlider] = useState(1);
 
-    useEffect(()=>{
+    useEffect(() => {
         let sliderAmount = slider.current.children.length;
         setSliderLength(sliderAmount);
-    },[]);
+        // setSliderLength(moviesMockup.length);
+    }, []);
 
     const sliderNext = () => {
-        if(sliderLength > 0){
-            currentSlider >= sliderLength 
-            ? setCurrentSlider(1) 
-            : setCurrentSlider(currentSlider + 1); 
-            // console.log(slider.current.children);
-            // const firstSlide = slider.current.children[0];
-            // const slideLength = slider.current.children[0].offsetWidth;
-            // slider.current.style.transition = `500ms ease-out all`;
-            // slider.current.style.transform = `translateX(-${slideLength}px)`;
-            // slider.current.style.transform = `translateX(-100%)`;
+
+        // currentSlider >= sliderLength
+            //     ? setCurrentSlider(1)
+            //     : setCurrentSlider(currentSlider + 1);
+
+        if (sliderLength > 0) {
+            currentSlider >= sliderLength
+                ? setCurrentSlider(1)
+                : setCurrentSlider(currentSlider + 1);
+
+        // con animacion de slider ..................................   
+            const firstSlide = slider.current.children[0];
+            const slideLength = slider.current.children[0].offsetWidth;
+            slider.current.style.transition = `500ms ease-out all`;
+            slider.current.style.transform = `translateX(-${slideLength}px)`;
+
+            const stopTransition = () => {
+                slider.current.style.transition = 'none';
+                slider.current.style.transform = 'translateX(0px)';
+                slider.current.appendChild(firstSlide);
+            }
+
+            slider.current.addEventListener('transitionend', stopTransition);
+
         }
     }
 
     const sliderPrev = () => {
-        if(slider.current.children.length > 0){
-            currentSlider <= 1 
-            ? setCurrentSlider(sliderLength) 
-            : setCurrentSlider(currentSlider - 1);
+        // if (slider.current.children.length > 0) {
+            currentSlider <= 1
+                ? setCurrentSlider(sliderLength)
+                : setCurrentSlider(currentSlider - 1);
             // console.log(slider.current.children);
             // const firstSlide = slider.current.children[0];
             // const slideLength = slider.current.children[0].offsetWidth;
             // slider.current.style.transition = `300ms ease-out all`;
             // slider.current.style.transform = `translateX(${slideLength}px)`;
             // slider.current.style.transform = `translateX(0px)`;
-        }
+        // }
     }
 
     return (
@@ -52,16 +90,17 @@ const Slider = () => {
                 <SliderBanner title={'spiderman'} banner={spiderman} />
                 <SliderBanner title={'Harry Potter Deadly Hollows 2'} banner={harry} />
                 <SliderBanner title={'Dune'} banner={dune} />
+                {/* <SliderBanner title={moviesMockup[currentSlider - 1].title} banner={moviesMockup[currentSlider - 1].banner} /> */}
             </div>
             <div className='slider__controls'>
-                <button 
+                <button
                     className='slider__button slider__button-left'
                     onClick={sliderPrev}
                 >
                     <MdOutlineArrowBackIosNew size={26} />
                 </button>
                 <span>{`${currentSlider} / ${sliderLength}`}</span>
-                <button 
+                <button
                     className='slider__button slider__button-right'
                     onClick={sliderNext}
                 >
