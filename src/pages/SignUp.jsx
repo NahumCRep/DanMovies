3ba5ccd43c3cae465baf594fd41cb565891aec18
Gisse88 <coronel.dg@gmail.com>
2022-02-08 15:42:31 -0300

    maquetando SignUp

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -13,51 +13,57 @@ const SignUp = () => {
             <div className="ingresar__container">
                 <div className="container login">
                     <form action='#'>
-                        <div className="login__form">
+                        <div className="form login">
                             <div className="title">Ingresar</div>
-                            <div className="inputBox">
-                                <MdPerson className="icon user"/>
-                                <input type="text" name="name" placeholder='Ingrese su usuario' required></input>
-                            </div>
-                            {/*
-                            <div className="inputBox">
-                                <MdMail className="icon mail"/>
-                                <input type="email" name="email" placeholder='Ingrese su email' required></input>
-                            </div>
-                            */}
-                            <div className="inputBox">
-                                <MdPassword className="icon password"/>
-                                <input type="password" name="password" placeholder='Ingrese su contraseña' required></input>
-                            </div>
-                            <div className="textPassword">alalala</div>
-                            <Button text='Ingresar'/>
-                        
-                        </div>
-                    </form>
 
-                </div>
-                <div className="container signup">
-                <form action='#'>
-                        <div className="login__form">
-                            <div className="title">Registrarse</div>
-                            <div className="inputBox">
-                                <MdPerson className="icon user"/>
-                                <input type="text" name="name" placeholder='Ingrese nombre de usuario' required></input>
+                            <div className="input__boxes">
+                                <div className="inputBox">
+                                    <MdPerson className="icon user"/>
+                                    <input type="text" name="name" placeholder='Ingrese su usuario' required></input>
+                                </div>
+                            
+                                <div className="inputBox">
+                                    <MdPassword className="icon password"/>
+                                    <input type="password" name="password" placeholder='Ingrese su contraseña' required></input>
+                                </div>
                             </div>
-                            <div className="inputBox">
-                                <MdMail className="icon mail"/>
-                                <input type="email" name="email" placeholder='Ingrese su email' required></input>
-                            </div>
-                            <div className="inputBox">
-                                <MdPassword className="icon password"/>
-                                <input type="password" name="password" placeholder='Ingrese su contraseña' required></input>
-                            </div>
-                            <div className="textPassword">alalala</div>
-                            <Button text='Registrarse'/>
+                            
+                            <div className="text">Olvidaste la contraseña?</div>
+                            <Button text='Ingresar'/>
+                            <div className="text">¿No tienes una cuenta?<label>Registrarse</label></div>
                         
                         </div>
                     </form>
                 </div>
+
+                <div className="container signup">
+                    <form action='#'>
+                        <div className="form signup">
+                            <div className="title">Registrarse</div>
+                           
+                           <div className="input__boxes">
+                                <div className="inputBox">
+                                    <MdPerson className="icon user"/>
+                                    <input type="text" name="name" placeholder='Ingrese nombre de usuario' required></input>
+                                </div>
+                                <div className="inputBox">
+                                    <MdMail className="icon mail"/>
+                                    <input type="email" name="email" placeholder='Ingrese su email' required></input>
+                                </div>
+                                <div className="inputBox">
+                                    <MdPassword className="icon password"/>
+                                    <input type="password" name="password" placeholder='Ingrese su contraseña' required></input>
+                                </div>
+
+                           </div>
+                            
+                            <Button text='Registrarse'/>
+                            <div className="text">¿Ya tienes una cuenta?<label>Ingresar</label></div>
+                        
+                        </div>
+                    </form>
+                </div>
+
             </div>
         </section>
     )
